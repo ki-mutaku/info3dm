5d64532a40c665a874e65c66439b5a963f954096
takuya <e245745@ie.u-ryukyu.ac.jp>
2026-04-28 09:22:18 +0900

    pytest and regression implemented

--- linear_regression/run.py
+++ linear_regression/run.py
@@ -1,6 +1,10 @@
 import datasets
+import regression
 
 X, Y = datasets.load_linear_example1()
-print(X)
-print(X[0])
-print(Y)
+model = regression.LinearRegression()
+
+print(model.x)
+# print(X)
+# print(X[0])
+# print(Y)
