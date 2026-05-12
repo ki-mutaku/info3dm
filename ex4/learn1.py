ae4f32422b8fa1ac38562d6eef5c26553ff32fd1
takuya <e245745@ie.u-ryukyu.ac.jp>
2026-05-12 11:47:36 +0900

    演習1.9終了

--- ex4/learn1.py
+++ ex4/learn1.py
@@ -2,10 +2,24 @@ from pathlib import Path
 
 import dataset1
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+# %%
 
 df = dataset1.read_tsv(dataset1.PROJECT_ROOT / Path("tsv/output.tsv"))
 
+# %%
+
 model = LinearRegression()
 
+# %%
+
+X = df["観測点"]
+y = df["観測値"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8, shuffle=False)
+
+
+# %%
 
 print(df)
+# %%
