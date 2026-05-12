74d9f0f734d0691f1b57a14ca96294e7b50b98b3
takuya <e245745@ie.u-ryukyu.ac.jp>
2026-05-12 11:31:13 +0900

    dataset1の新仕様に対応

--- ex4/learn1.py
+++ ex4/learn1.py
@@ -1,7 +1,9 @@
+from pathlib import Path
+
 import dataset1
 from sklearn.linear_model import LinearRegression
 
-df = dataset1.read_tsv("./ex4/output.tsv")
+df = dataset1.read_tsv(dataset1.PROJECT_ROOT / Path("tsv/output.tsv"))
 
 model = LinearRegression()
 
